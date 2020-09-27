992d7a57b8ab0fa2952df9331243d67519e17814
unknown <espenstokke98@gmail.com>
2020-09-27 21:15:16 +0200

    can now query reviews by userid and university

--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,8 @@
 from flask import request, abort
 from server.study_data import Study, University
 from server.models import Review
-from server import app 
+from server.verification import review_exists
+from server import app, db
 
 uni = University()
 all_unis = uni.get_all_universities_json()
@@ -31,3 +32,36 @@ def studies():
             abort(404, description="Resource not found")
 
     return all_studies
+
+
+@app.route('/universities/reviews', methods=["GET", "POST"])
+def reviews():
+    if request.method == "POST":
+        data = request.json
+
+        if review_exists(data["user_id"], data["uni_code"]):
+            abort(403, description="Review already exists") # Replace this code with deleting existing entry and storing new one
+
+        review = Review(user_id=data["user_id"], uni_code=data["uni_code"], rating=data["rating"])
+        db.session.add(review)
+        db.session.commit()
+        return f"Posted {review}"
+    
+    if request.method == "GET":
+        user_query = request.args.get("user_id")
+        uni_query = request.args.get("uni_code").upper()
+
+        if user_query and uni_query:
+            review = Review.query.filter_by(user_id=user_query, uni_code=uni_query).first()
+            return str(review)
+
+        if user_query:
+            review = Review.query.filter_by(user_id=user_query).all()
+            return str(review)
+
+        if uni_query:
+            review = Review.query.filter_by(uni_code=uni_query).all()
+            return str(review)
+
+        reviews = Review.query.all()
+        return str(reviews)
